8b9c781e3b9065cf62f0d3c0c286f2d078430f76
Shivansh Vij <shivanshvij@loopholelabs.io>
2022-02-24 19:46:11 -0500

    Adding encryption

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,13 +1,45 @@
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
+import { EncryptJWT, jwtDecrypt, importJWK } from "jose"
+import uuid from "uuid"
+import { Buffer } from 'buffer';
+import { now } from "next-auth/client/_utils";
+import crypto from "crypto"
+
+const JWK = importJWK(JSON.parse(process.env.NEXTAUTH_ENCRYPT))
+
+async function getJWK() {
+  return await JWK;
+}
 
 export default NextAuth({
-  // Configure one or more authentication providers
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
     }),
-    // ...add more providers here
   ],
+  session: {
+    strategy: "jwt",
+  },
+  jwt: {
+    encrypt: false,
+    async encode({token,  secret,  maxAge}) {
+      const encryptionSecret = await getJWK()
+      return await new EncryptJWT(token)
+          .setProtectedHeader({ alg: "dir", enc: "A256GCM" })
+          .setIssuedAt()
+          .setExpirationTime(now() + maxAge)
+          .setJti(crypto.randomUUID ? crypto.randomUUID() : uuid())
+          .encrypt(encryptionSecret)
+    },
+    async decode({token,  secret}) {
+      if (!token) return null
+      const encryptionSecret = await getJWK()
+      const { payload } = await jwtDecrypt(token, encryptionSecret, {
+        clockTolerance: 15,
+      })
+      return payload
+    },
+  }
 })
